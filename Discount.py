2bfe6511396f42d997ed165e978f9c3a1468b579
Albertmutai <55190986+Albertmutai@users.noreply.github.com>
2024-09-10 16:17:54 +0300

    Update Discount.py

--- Discount.py
+++ Discount.py
@@ -1,6 +1,6 @@
 def calculate_discount(price, discount_percent):
-    # Check if the discount percentage is 20% or higher
-    if discount_percent >= 20:
+    # Check if the discount percentage is 15% or higher
+    if discount_percent >= 15:
         # Calculate the discounted price
         discounted_price = price - (price * (discount_percent / 100))
         return discounted_price
@@ -9,13 +9,14 @@ def calculate_discount(price, discount_percent):
         return price
 
 # Prompt the user to enter the original price and discount percentage
-original_price = float(input("Enter the original price of the item: Kes"))
+original_price = float(input("Enter the original price of the item: $"))
 discount_percent = float(input("Enter the discount percentage: "))
 
 # Calculate the final price using the calculate_discount function
 final_price = calculate_discount(original_price, discount_percent)
 
 # Display the result
+
 if final_price == original_price:
     print(f"Not discounted. Final Price: ${final_price}")
 else:
